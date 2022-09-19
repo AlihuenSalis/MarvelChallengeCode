506d4e5c9756f6579061f1cf922ea5e6d194c4a6
Alihuem <alihuen.salis@gmail.com>
2022-09-19 12:55:09 -0300

    add login info

--- app/src/main/java/com/example/challengecode/Application.kt
+++ app/src/main/java/com/example/challengecode/Application.kt
@@ -1,7 +1,21 @@
 package com.example.challengecode
 
 import android.app.Application
+import com.example.challengecode.domain.model.user.User
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application() {
+
+    companion object {
+        val PREFS = "challenge.conf"
+        val PREFSREMEMBER = "challenge.remember"
+    }
+
+    fun loadCredentials(): User {
+        val prefs = getSharedPreferences(PREFS, MODE_PRIVATE)
+        val usernameShared = prefs.getString("username", null)
+        val passwordShared = prefs.getString("password", null)
+        return User(usernameShared, passwordShared)
+    }
+}
--- app/src/main/java/com/example/challengecode/ui/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/challengecode/ui/view/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.challengecode.ui.view.activities
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.challengecode.R
 import com.example.challengecode.databinding.ActivityMainBinding
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,10 +30,46 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-        ))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Setup
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(provider ?: "")
+
+        // Guardado de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+            .edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
+
     }
+
+    private fun setup(provider: String) {
+//        binding.btnSignOut.setonClickListener {
+//
+//            val prefs =
+//                getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+//            prefs.clear()
+//            prefs.apply()
+//
+//            if (provider == ProviderType.FACEBOOK.name) {
+//                LoginManager.getInstance().logOut()
+//            }
+//
+//            FirebaseAuth.getInstance().signOut()
+//            onBackPressed()
+//        }
+    }
+
+    override fun onBackPressed() {}
 }
